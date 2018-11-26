71c1310e9a39c909fcd99b6538ca8af00a1533e1
Bryant Kelley <bryantkelley14@icloud.com>
2018-11-25 18:15:20 -0600

    Added prompt for RID and message. Added send sockets for new and forwarding messages.

--- Slave.py
+++ Slave.py
@@ -18,17 +18,17 @@ def main(argv):
         sys.exit()
 
     # Send Join Ring Request
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((master_hostname, int(master_port)))
+    s_join = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s_join.connect((master_hostname, int(master_port)))
     
     request = ''.join(chr(x) for x in [13, 0x4A, 0x6F, 0x79, 0x21])
-    s.sendall(request) 
+    s_join.sendall(request)
 
     response = s.recv(4096)
 
     print(response)
     # Set self as slave node on ring and get ring ID
-    master_gid = 0 # byte_to_int(response[0])
+    master_gid = byte_to_int(response[0])
     # magic_number = int(str(response[1]) + str(response[2]) + str(response[3]) + str(response[4]))
     thisRID = byte_to_int(response[5])
     # nextSlaveIP = int(str(response[6]) + str(response[7]) + str(response[8]) + str(response[9]))
@@ -40,13 +40,27 @@ def main(argv):
     print("Next Slave: ", nextSlave_pretty)
 
     # TODO: Prompt user for a ring ID and message
+    newRID = ""
+    while newRID == "":
+        rawRID = raw_input('Enter a Ring ID: ')
+        try:
+            newRID = int(rawID)
+        except:
+            newRID = ""
+            print("Invalid Ring ID")
+    raw_message = raw_input('Enter your message: ')
 
     # TODO: Send message to node
+    s_send =  socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    S_send.connect((nextSlave_pretty, int(10010 + master_gid * 5 + thisRID - 1)))
+    
+
 
     # TODO: Display any packet addressed to this node
 
     # TODO: Forward any other packet with TTL > 1
-    
+    s_forward = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    S_forward.connect((nextSlave_pretty, int(10010 + master_gid * 5 + thisRID - 1)))
 
 if __name__ == '__main__':
 
