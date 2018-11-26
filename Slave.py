0fefe92887f4ab423148cfb4ecd826a72f7f4f96
Bryant Kelley <bryantkelley14@icloud.com>
2018-11-25 18:23:09 -0600

    Changed variable names to meet PEP standards. Fixed typos.

--- Slave.py
+++ Slave.py
@@ -3,16 +3,18 @@
 import socket
 import sys
 
+
 def byte_to_int(byte):
     integer = 0
     for b in byte:
         integer += ord(b)
     return int(integer)
 
+
 def main(argv):
     try:
         master_hostname = argv[1]
-        master_port = argv[2] # for us this should be 10075
+        master_port = argv[2]  # for us this should be 10075
     except:
         print("Invalid arguments.")
         sys.exit()
@@ -30,41 +32,40 @@ def main(argv):
     # Set self as slave node on ring and get ring ID
     master_gid = byte_to_int(response[0])
     # magic_number = int(str(response[1]) + str(response[2]) + str(response[3]) + str(response[4]))
-    thisRID = byte_to_int(response[5])
+    this_rid = byte_to_int(response[5])
     # nextSlaveIP = int(str(response[6]) + str(response[7]) + str(response[8]) + str(response[9]))
-    nextSlave_pretty = str(byte_to_int(response[6])) + "." + str(byte_to_int(response[7])) + "." + str(byte_to_int(response[8])) + "." + str(byte_to_int(response[9]))
+    next_slave_pretty = str(byte_to_int(response[6])) + "." + str(byte_to_int(response[7])) + "." + str(byte_to_int(response[8])) + "." + str(byte_to_int(response[9]))
 
     # Display the GID of master, own ring ID, and the IP address of next slave
     print("Master GID: ", master_gid)
-    print("My Ring ID: ", thisRID)
-    print("Next Slave: ", nextSlave_pretty)
+    print("My Ring ID: ", this_rid)
+    print("Next Slave: ", next_slave_pretty)
 
     # TODO: Prompt user for a ring ID and message
-    newRID = ""
-    while newRID == "":
-        rawRID = raw_input('Enter a Ring ID: ')
+    new_rid = ""
+    while new_rid == "":
+        raw_rid = raw_input('Enter a Ring ID: ')
         try:
-            newRID = int(rawID)
+            new_rid = int(raw_rid)
         except:
-            newRID = ""
+            new_rid = ""
             print("Invalid Ring ID")
     raw_message = raw_input('Enter your message: ')
 
     # TODO: Send message to node
-    s_send =  socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    S_send.connect((nextSlave_pretty, int(10010 + master_gid * 5 + thisRID - 1)))
+    s_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s_send.connect((next_slave_pretty, int(10010 + master_gid * 5 + this_rid - 1)))
     
-
 
     # TODO: Display any packet addressed to this node
 
     # TODO: Forward any other packet with TTL > 1
     s_forward = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    S_forward.connect((nextSlave_pretty, int(10010 + master_gid * 5 + thisRID - 1)))
+    s_forward.connect((next_slave_pretty, int(10010 + master_gid * 5 + this_rid - 1)))
+
 
 if __name__ == '__main__':
 
     print(byte_to_int(''))
 
     main(sys.argv)
-
